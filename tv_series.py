e12b5f744ebb90faa1ecff6e8fa3bd6ac54d7912
Alessandro Donati <128533206+alelallox@users.noreply.github.com>
2023-03-28 10:55:53 +0200

    Update tv_series.py

--- tv_series.py
+++ tv_series.py
@@ -7,7 +7,7 @@ anno_corrente = datetime.datetime.now().year
 
 #information for the text file
 titolo_file = "serie_tv_" + str(anno_corrente)
-directory = "your directory"
+directory = "C:/Users/aless/Desktop/python/tv series/file tv series"
 
 dizionario = {}
 
@@ -19,12 +19,20 @@ if os.path.exists("dizionario.pickle"):
 #command definition ("tv series", "vote", "pop", "pop tot" and "save")
 while True:
     key = input("Inserisci una serie tv o scrivi comando: ")
-    if key == "end":
-        break
+    if key == "print":
+        #print the table with all TV series reordered 
+        dizionario_ordinato = {k: v for k, v in sorted(dizionario.items(), key=lambda item: item[1], reverse=True)}
+        print("")
+        print("{:<28} {:<28}".format('serie', 'voto'))
+        print('-' * 34)
+        for key, value in dizionario_ordinato.items():
+            print("{:<28} {:<28}".format(key, value))    
     elif key == "pop":
         dizionario.popitem()
     elif key == "pop tot":
         dizionario.clear()
+    elif key == "end":
+        break
     elif key == "save":
         #save creates a text file containing the table ordered with all tv series
         dizionario_ordinato = {k: v for k, v in sorted(dizionario.items(), key=lambda item: item[1], reverse=True)}
@@ -41,11 +49,3 @@ while True:
 #save the updated dictionary to the file
 with open("dizionario.pickle", "wb") as f:
     pickle.dump(dizionario, f)
-
-#print the table with all TV series reordered 
-dizionario_ordinato = {k: v for k, v in sorted(dizionario.items(), key=lambda item: item[1], reverse=True)}
-print("")
-print("{:<28} {:<28}".format('serie', 'voto'))
-print('-' * 34)
-for key, value in dizionario_ordinato.items():
-    print("{:<28} {:<28}".format(key, value))
